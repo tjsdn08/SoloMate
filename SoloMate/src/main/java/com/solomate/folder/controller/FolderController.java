f1c1718b39a0be0b748a58a618f919fa27dc8e19
User <User@HYW>
2026-03-30 17:54:30 +0900

    feat: FolderController 수정

--- SoloMate/src/main/java/com/solomate/folder/controller/FolderController.java
+++ SoloMate/src/main/java/com/solomate/folder/controller/FolderController.java
@@ -1,5 +1,15 @@
 package com.solomate.folder.controller;
 
-public class FolderController {
+import com.solomate.main.controller.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class FolderController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
